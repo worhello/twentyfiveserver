95df84a32adb5a0dbc1fabb870a8d55db0bf7959
worhello <worhello@users.noreply.github.com>
2021-11-11 22:32:04 +0000

    More testing (#40)

--- test/playerNotifierModelTests.js
+++ test/playerNotifierModelTests.js
@@ -5,7 +5,12 @@ const playerNotifierModel = require('../model/playerNotifierModel');
 let assert = require('assert');
 
 describe('playerNotifierModelTests - basic flows', function() {
+    let gameId = "gameId";
+    beforeEach(function() {
+        playerNotifierModel.deleteGameNotifiers(gameId);
+        assert.strictEqual(playerNotifierModel.getPlayerNotifiersForGame(gameId), undefined);
+    })
     it ('nothing in model - null returned', function() {
-        assert.strictEqual(playerNotifierModel.getPlayerNotifiersForGame("gameId"), undefined);
+        assert.strictEqual(playerNotifierModel.getPlayerNotifiersForGame(gameId), undefined);
     });
 });
